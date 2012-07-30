115c0d04dafdebce07b58204c00a7a7f961a7133
W Li <wqli@hotmail.co.uk>
2012-07-30 22:04:29 +0100

    modified:   networkAlg.m 	modified:   neuralNetworkTest.m

--- neuralNetwork/networkAlg.m
+++ neuralNetwork/networkAlg.m
@@ -1,24 +1,25 @@
 function networkAlg(samples, groundtruth)
 
 figure1 = figure;
-rate = .1;
+rate = .001;
 % adding bias term to samples
 X = [ones(1, size(samples, 2)); samples];
 
 % initial weights
-d = [3 3 1]; % number of cells in each layer
+d = [3 3 3 3 1]; % number of cells in each layer
 L = size(d, 2); % number of layers
-W{1} = -1 + 2 * rand(size(X,1), d(1));
+W{1} = -0.5 + rand(size(X,1), d(1));
 for l = 2 : L
 	W{l} = rand(d(l-1), d(l));
 end
 
-index = 0;
-while index < 10000000
+index = 1;
+errornum = 16
+while errornum > 15
 
 % stochastic gradient descent
 % select a random sample
-n = randsample(size(X,2), 1);
+n = mod(index, size(samples,2)) +1;
 x = X(:,n);
 
 % init space for all out put and derivatives
@@ -51,13 +52,24 @@ for l = 1:L
 end
 
 % draw a map of weights
-if mod(index, 1000) == 0
-plotAll(figure1, samples, groundtruth, W);
-drawnow;
+if errornum < 100
+    plotAll(figure1, samples, groundtruth, W);
+    drawnow;
 end
+
 % loop index update
 index = index + 1;
-fprintf('%d\n', index);
+%fprintf('%d\n', index);
+
+result = [];
+for t = 1:size(X, 2)
+    [~, valu] = evalNetwork(X(:,t), W);
+    result(t) = valu;
+end
+result = result > 0;
+result = result*2 - 1;
+errornum = sum((result'-groundtruth)~=0)
+
 end % end of while loop
 end % end of function
 
@@ -76,17 +88,13 @@ function plotAll(figHandle, samples, groundtruth, weights)
 axes1 = axes('parent', figHandle);
 xlim(axes1, [-8, 12]);
 ylim(axes1, [-8, 12]);
-plotArea(figHandle, weights);
 hold on;
 plot(samples(1, groundtruth == 1), samples(2, groundtruth==1), '+', ...
 	'Color', [1 0 0], 'linewidth', 2);
 hold on
 plot(samples(1, groundtruth == -1), samples(2, groundtruth==-1), '+',...
-	'Color', [0 0 1], 'linewidth', 2);
+	'Color', [0 0.75 0], 'linewidth', 2);
 hold on;
-end
-
-function plotArea(figHandle, weights)
 
 [x1 x2] = meshgrid(-8:0.5:12, -8:0.5:12);
 y = zeros(size(x1));
@@ -95,15 +103,15 @@ for i = 1:size(x1, 1)
 		x = [1; x1(i,j);x2(i,j)];
 		[~, r] = evalNetwork(x, weights);
 		if r > 0
-			y(i,j) = 1;
+			y(i,j) = r;
 		else
-			y(i,j) =-1;
+			y(i,j) =r;
 		end
 	end
 end
-contourf(x1, x2, y, 2);
-hold on;
+contour(x1, x2, y, 1, 'linewidth', 2.0, 'Color', [0 0 0.75]);
 end
+
 
 function [out, y] = evalNetwork(x, W)
 out = cell(size(W));
--- neuralNetwork/neuralNetworkTest.m
+++ neuralNetwork/neuralNetworkTest.m
@@ -1,31 +1,33 @@
 clear all; close all;
 % Gaussian a
-numOfA = 600;
+numOfA = 500;
 muA = [6, 4];
 sigmaA = [3 0;0  4];
 dataA = mvnrnd(muA, sigmaA, numOfA);
 
 % Gaussian b
-numOfB = 1;
+numOfB = 100;
 muB = [3, 0];
 sigmaB = [2 2.5; 2.5 5];
 dataB = mvnrnd(muB, sigmaB, numOfB);
 
 % Gaussian c
-numOfC = 600;
+numOfC = 500;
 muC = [-1, -1];
 sigmaC = [2 2.5; 2.5 5];
 dataC = mvnrnd(muC, sigmaC, numOfC);
 
 % Gaussian d
-numOfD = 1;
+numOfD = 100;
 muD = [5, 1];
 sigmaD = [2 2.5; 2.5 5];
 dataD = mvnrnd(muD, sigmaD, numOfD);
 
+%samples = [dataA; dataB; dataC; dataD]';
+%targets = [ones(numOfA+numOfB, 1); -1*ones(numOfC+numOfD,1)];
+
 samples = [dataA; dataC;]';
 targets = [ones(numOfA, 1); -1*ones(numOfC,1)];
-
 %plot(samples(1, targets==1), samples(2, targets==1), '+',...
 %	'Color', [1 0 0], 'linewidth', 1.8);
 %hold on;
