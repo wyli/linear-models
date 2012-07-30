8192686fe7ee5b9548f59e2b91daf8c41773ae07
W Li <wqli@hotmail.co.uk>
2012-07-30 12:36:59 +0100

    modified:   logistic/logisticReg.m

--- logistic/logisticReg.m
+++ logistic/logisticReg.m
@@ -1,4 +1,5 @@
 function W = logisticReg(samples, targets)
+
 figure1 = figure;
 % set rate
 rate = 0.3;
@@ -27,16 +28,22 @@ while sum(grad.^2) > 0.0002
 end
 
 function plotAll(figHandler, samples, groundtruth, weights)
+% x y scale
 axes1 = axes('parent', figHandler);
 xlim(axes1, [-4, 10]);
 ylim(axes1, [-4, 10]);
 hold on;
-plot(samples(1, groundtruth == 1), samples(2,groundtruth==1), '+', 'Color', [1 0 0], 'linewidth', 2);
+
+% plot points
+plot(samples(1, groundtruth == 1), samples(2,groundtruth==1), '+',...
+	'Color', [1 0 0], 'linewidth', 2);
 hold on;
-plot(samples(1, groundtruth == -1), samples(2, groundtruth==-1), '+', 'Color', [0 0.5 0], 'linewidth', 2);
+plot(samples(1, groundtruth == -1), samples(2, groundtruth==-1), '+',...
+	'Color', [0 0.5 0], 'linewidth', 2);
 hold on;
+
+% plot boundary
 t = -4:0.5:10;
 plot(t, (-weights(1) - weights(2)*t)/weights(3), 'blue', 'linewidth', 2);
 end
 end
-
